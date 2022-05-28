0d916302a521b0c3af69cd209ea9c93da8d1cb48
k2 <kazumoto.kondo@gmail.com>
2022-05-28 21:54:39 +0900

    ok

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-ln -sf /User/k/etc/dotfiles/.zshrc ~/.zshrc
+# make symbolic link
+ln -sf /Users/k/etc/dotfiles/.zshrc ~/.zshrc
+# check link
 ls -al ~/.zshrc
+# check file
+head ~/.zshrc
 
 #ln -s .vimrc ~/.vimrc
 
