9b2773e178c88b15d391b20e983a2ba0462e30c3
etalli <kazumoto.kondo@gmail.com>
2024-05-19 18:23:03 +0900

    next

--- install.sh
+++ install.sh
@@ -3,15 +3,9 @@
 ## zshrc
 # make symbolic link
 ln -sf /Users/k2/etc/dotfiles/.zshrc ~/.zshrc
-# check link
-ls -al ~/.zshrc
-# check file
-head ~/.zshrc
 
 ## vimrc
 # make symbolic link
 ln -sf /Users/k2/etc/dotfiles/.vimrc ~/.vimrc
-# check link
-ls -al ~/.vimrc
-# check file
-head ~/.vimrc
+
+echo "done"
