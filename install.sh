d8034a46ecc197ec16ef6c9bfcfdff83fad79f98
k2 <kazumoto.kondo@gmail.com>
2025-10-26 21:53:45 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ set -x
 
 ## zshrc
 # make symbolic link
-ln -sf /Users/k2/etc/dotfiles/.zshrc ~/.zshrc
+ln -sf $HOME/etc/dotfiles/.zshrc ~/.zshrc
 
 ## vimrc
 # make symbolic link
-ln -sf /Users/k2/etc/dotfiles/.vimrc ~/.vimrc
+ln -sf $HOME/etc/dotfiles/.vimrc ~/.vimrc
 
 ## aliases
-ln -sf /Users/k2/etc/dotfiles/.zsh_aliases ~/.zsh_aliases
+ln -sf $HOME/etc/dotfiles/.zsh_aliases ~/.zsh_aliases
 
 set +x
 echo "done"
